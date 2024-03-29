187f2d66951586a174f18aacf87ba7417921522d
SuryodayABM <suryoday.yadav@abmindia.com>
2024-03-29 15:03:52 +0530

    chnages in eWatch Component, making stars on unsplash

--- src/LandingPages/eWatch/FontCircle.js
+++ src/LandingPages/eWatch/FontCircle.js
@@ -7,8 +7,39 @@ function FontCircle(){
         <FontCirclediv>
             <img src={watch} alt="watch"/>
             <FontCircleText>
-                <span style={{position: 'relative', width: '9px', height: '16px', rotate: '149.74', top: '87.12px', left: '13.24px', lineHeight:'16.35px'}}>L</span>
-            {/* Lorem ipsum dolor sit amet, cons */}
+                <span style={{letterSpacing: '6px', fontSize: '12px',  position: 'absolute', transform: 'rotate(24deg) translate(40px) rotate(-24deg)'}}>L</span>
+                <span style={{letterSpacing: '6px', fontSize: '12px', position: 'absolute', transform: 'rotate(54deg) translate(40px) rotate(-54deg)'}}>o</span>
+                <span style={{letterSpacing: '6px', fontSize: '12px',  position: 'absolute', transform: 'rotate(84deg) translate(40px) rotate(-84deg)'}}>r</span>
+                <span style={{letterSpacing: '6px', fontSize: '12px',  position: 'absolute', transform: 'rotate(114deg) translate(40px) rotate(-114deg)'}}>e</span>
+                <span style={{letterSpacing: '6px', fontSize: '12px',  position: 'absolute', transform: 'rotate(144deg) translate(40px) rotate(-144deg)'}}>m</span>
+                <span style={{letterSpacing: '6px', fontSize: '12px',  position: 'absolute', transform: 'rotate(174deg) translate(40px) rotate(-174deg)'}}>r</span>
+                <span style={{letterSpacing: '6px', fontSize: '12px',  position: 'absolute', transform: 'rotate(204deg) translate(40px) rotate(-204deg)'}}>i</span>
+                <span style={{letterSpacing: '6px', fontSize: '12px',  position: 'absolute', transform: 'rotate(234deg) translate(40px) rotate(-234deg)'}}>p</span>
+                <span style={{letterSpacing: '6px', fontSize: '12px',  position: 'absolute', transform: 'rotate(274deg) translate(45px) rotate(-274deg)'}}>s</span>
+                <span style={{letterSpacing: '6px', fontSize: '12px',  position: 'absolute', transform: 'rotate(304deg) translate(45px) rotate(-304deg)'}}>u</span>
+                <span style={{letterSpacing: '6px', fontSize: '12px',  position: 'absolute', transform: 'rotate(334deg) translate(45px) rotate(-334deg)'}}>m</span>
+                <span style={{letterSpacing: '6px', fontSize: '12px',  position: 'absolute', transform: 'rotate(360deg) translate(45px) rotate(-360deg)'}}>d</span>
+                {/* <span style={{letterSpacing: '6px', fontSize: '12px',  position: 'absolute', transform: 'rotate(184deg) translate(45px) rotate(-184deg)'}}>o</span>
+                <span style={{letterSpacing: '6px', fontSize: '12px',  position: 'absolute', transform: 'rotate(196deg) translate(45px) rotate(-196deg)'}}>l</span>
+                <span style={{letterSpacing: '6px', fontSize: '12px',  position: 'absolute', transform: 'rotate(208deg) translate(45px) rotate(-208deg)'}}>o</span>
+                <span style={{letterSpacing: '6px', fontSize: '12px',  position: 'absolute', transform: 'rotate(220deg) translate(45px) rotate(-220deg)'}}>r</span>
+                <span style={{letterSpacing: '6px', fontSize: '12px',  position: 'absolute', transform: 'rotate(232deg) translate(45px) rotate(-232deg)'}}>s</span> */}
+                {/* <span style={{letterSpacing: '6px', fontSize: '12px',  position: 'absolute', transform: 'rotate(244deg) translate(45px) rotate(-244deg)'}}>i</span>
+                <span style={{letterSpacing: '6px', fontSize: '12px',  position: 'absolute', transform: 'rotate(256deg) translate(45px) rotate(-256deg)'}}>t</span>
+                <span style={{letterSpacing: '6px', fontSize: '12px',  position: 'absolute', transform: 'rotate(268deg) translate(45px) rotate(-268deg)'}}>a</span>
+                <span style={{letterSpacing: '6px', fontSize: '12px',  position: 'absolute', transform: 'rotate(280deg) translate(45px) rotate(-280eg)'}}>m</span>
+                <span style={{letterSpacing: '6px', fontSize: '12px',  position: 'absolute', transform: 'rotate(292deg) translate(45px) rotate(-292deg)'}}>e</span>
+                <span style={{letterSpacing: '6px', fontSize: '12px',  position: 'absolute', transform: 'rotate(304deg) translate(45px) rotate(-304deg)'}}>t</span>
+                <span style={{letterSpacing: '6px', fontSize: '12px',  position: 'absolute', transform: 'rotate(316deg) translate(45px) rotate(-316deg)'}}>,</span>
+                <span style={{letterSpacing: '6px', fontSize: '12px',  position: 'absolute', transform: 'rotate(328deg) translate(45px) rotate(-328deg)'}}>c</span>
+                <span style={{letterSpacing: '6px', fontSize: '12px',  position: 'absolute', transform: 'rotate(340deg) translate(45px) rotate(-340deg)'}}>o</span>
+                <span style={{letterSpacing: '6px', fontSize: '12px',  position: 'absolute', transform: 'rotate(352deg) translate(45px) rotate(-352deg)'}}>n</span>
+                <span style={{letterSpacing: '6px', fontSize: '12px',  position: 'absolute', transform: 'rotate(364deg) translate(45px) rotate(-364deg)'}}>s</span> */}
+
+
+
+                
+
             </FontCircleText>
         </FontCirclediv>
     )
--- src/LandingPages/eWatch/ImgGroup.jsx
+++ src/LandingPages/eWatch/ImgGroup.jsx
@@ -1,19 +1,35 @@
 import React from 'react';
-import { BoxStar, ImageGroupBox, Stroke1, Stroke2, Unsplash } from './Styled';
+import { BoxStar, ImageContainer, ImageGroupBox, Star1, Star2, Stroke1, Stroke2, Tag1, Tag2, Unsplash } from './Styled';
 import star from './assets/Star.png'
+import start1 from './assets/star (1).png'
+import star2 from './assets/Star (2).png'
 import unsplash from './assets/unsplash_QRWAdBCqysc (1).png'
+import { Grid } from '@mui/material';
 
 
-function ImageGroup(){
-    return(
+function ImageGroup() {
+    return (
         <ImageGroupBox>
             <Stroke1>
                 <BoxStar>
-                    <img src={star} alt='star'/>
+                    <img src={star} alt='star' />
                 </BoxStar>
             </Stroke1>
             <Unsplash>
-                <img src={unsplash} alt='unsplash'/>
+                <Grid container spacing={0}>
+                    <ImageContainer item xs={12}>
+                        <img src={unsplash} alt='unsplash' />
+                        <Star1>
+                            <img src={start1} alt='star1'/>
+                        </Star1>
+                        <Star2>
+                            <img src={star2} alt='star2'/>
+                        </Star2>
+                        <Tag1>Award Winning Product</Tag1>
+                        <Tag2>Ergonomic Design</Tag2>
+                    </ImageContainer>
+                </Grid>
+
             </Unsplash>
             <Stroke2 />
         </ImageGroupBox>
