cddfdeb06a8b975501fcfedd4f6c3846f09ddc0c
SuryodayABM <suryoday.yadav@abmindia.com>
2024-04-17 14:43:27 +0530

    making default as Tea-cofee

--- src/LandingPages/index.js
+++ src/LandingPages/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
-// import TeaCofee from './TeaCoffee';
- //import CardDesignPro from './cardesign';
-import Nature from './Nature';
-//  import EWatch from './eWatch';
+import TeaCofee from './TeaCoffee';
+//  import CardDesignPro from './cardesign';
+// import Nature from './Nature';
+//   import EWatch from './eWatch';
 
 function LandingPages(){
     return(
       <React.Fragment>
-        <Nature />
-        {/* <EWatch />
-        <CardDesignPro />
-        <TeaCofee /> */}
+        {/* <Nature />
+        <EWatch />
+        <CardDesignPro /> */}
+        <TeaCofee />
       </React.Fragment>
     )
 }
